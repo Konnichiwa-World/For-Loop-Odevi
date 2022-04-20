786c1b6570da4d74799eb8793aae336cef1a0116
Onur MUTLU <103672350+Konnichiwa-World@users.noreply.github.com>
2022-04-21 00:05:21 +0300

    0-100 aralığında, girilen 2 pozitif tamsayı arasındaki asal sayıları bulan uygulama
    Uygulamanın çalışma mantığı; 0-100 aralığında iki sayı girildiğinde, mesela 15 ve 85 girdiniz, bu iki sayı arasındaki asal sayıları ekrana yazıyor.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace forx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lutfen tek yada iki basamaklı pozitif, ilki diğerinden düşük oalcak şekilde 2 farklı sayi giriniz:");
+            Int32 x = Int32.Parse(Console.ReadLine()); //diyelim ki 5
+            Int32 y = Int32.Parse(Console.ReadLine()); //diyelim ki 10
+
+            if(x<0||x/100>=1){System.Console.WriteLine("Lütfen 3 ve daha fazla basamakli sayilar girmeyiniz. 2 basamagi asmasin.");}
+            else if(y<0||y/100>=1){System.Console.WriteLine("Lütfen 3 ve daha fazla basamakli sayilar girmeyiniz. 2 basamagi asmasin.");}
+            else if(x>=y){System.Console.WriteLine("ilki ikincisinden düşük olmali");}
+            else{
+            int a;
+            for( a=x; a<=y; a++)
+            {
+
+                
+              if(a<12) {
+                    if(a==2){ System.Console.WriteLine("2");}
+                    
+                    if(a==3){System.Console.WriteLine("3");}
+                    
+                    if(a==5){System.Console.WriteLine("5");}
+                    
+                    if(a==7){System.Console.WriteLine("7");}
+                    
+                    if(a==11){System.Console.WriteLine("11");}
+
+                    else{continue;};
+                    }
+                
+                else
+                {
+
+                if(a%2==0){continue;}
+                if(a%3==0){continue;}
+                if(a%5==0){continue;}
+                if(a%7==0){continue;}
+                if(a%11==0){continue;}}
+                
+                Console.WriteLine(a);}
+            }
+        }
+    }
+}
